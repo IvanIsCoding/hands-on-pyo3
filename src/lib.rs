6d4e1b581c5fd061d756075b200205b619b6b58e
Ivan Carvalho <ivancarvalho@gatech.edu>
2025-10-05 17:33:06 -0400

    Improve code comments

--- src/lib.rs
+++ src/lib.rs
@@ -11,20 +11,22 @@ fn decode_jxl_bytes<'py>(
     py: Python<'py>,
     jxl_bytes: &Bound<'py, PyBytes>,
 ) -> PyResult<Bound<'py, PyArray3<u8>>> {
-    // Extract bytes from PyBytes
+    // Extract bytes from PyBytes to a Rust data struct jxl-oxide can read.
+    // Manipulating Python data into a format Rust accepts is common in PyO3 code.
     let bytes = jxl_bytes.as_bytes();
-
-    // Create cursor and call core logic
     let cursor = std::io::Cursor::new(bytes);
+
+    // This method reads the image in a ndarray::Array3. Implmentation details
+    // are not relevant.
     let array = decode_jxl_core(cursor).map_err(|e| pyo3::exceptions::PyValueError::new_err(e))?;
 
-    // Convert to PyArray3 and return
+    // Convert to a NumPy array and return.
     Ok(PyArray3::from_array(py, &array))
 }
 
-/// Python module
+/// Declare modules and functions. This will be imported in Python as `import jxl_demo.jxl_demo_rs`
 #[pymodule]
-fn jxl_decoder(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn jxl_demo_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(decode_jxl_bytes, m)?)?;
     Ok(())
 }
